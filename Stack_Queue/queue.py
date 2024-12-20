c899b342f3d23b3b0c40bf6b653e85630364317b
Inaba Kenji <k.inaba040332@gmail.com>
2024-12-20 12:59:01 +0900

    [finish] キューを並びを逆順にする方法の完了

--- Stack_Queue/queue.py
+++ Stack_Queue/queue.py
@@ -11,6 +11,16 @@ class Queue:
             return self.queue.pop(0)
         raise IndexError("キューが空です")
 
+    def reverse1(self):
+        new_queue = Queue()
+        while self.queue:
+            new_queue.enqueue(self.queue.pop())
+        return new_queue.queue
+
+    def reverse2(self):
+        self.queue.reverse()
+        return self.queue
+
 
 if __name__ == "__main__":
     q = Queue()
@@ -19,8 +29,10 @@ if __name__ == "__main__":
     q.enqueue(3)
     q.enqueue(4)
     q.enqueue(5)
-    print(q.queue)
-    print(q.dequeue())
-    print(q.dequeue())
-    print(q.dequeue())
-    print(q.queue)
+    # print(q.queue)
+    # print(q.dequeue())
+    # print(q.dequeue())
+    # print(q.dequeue())
+    # print(q.queue)
+    # print(q.reverse1())
+    print(q.reverse2())
